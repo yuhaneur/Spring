67558f02f73c8ee8e9baa55cf42f80a8facaad91
yuhaneur <yuhaneur@naver.com>
2024-03-02 00:45:41 +0900

    이미지 비동기화 처리

--- homework/src/main/webapp/resources/js/app/04/imageForm1.js
+++ homework/src/main/webapp/resources/js/app/04/imageForm1.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-document.forms[0].addEventListener("submit",(event)=>{
+/*document.forms[0].addEventListener("submit",(event)=>{
 	event.preventDefault();
 	fetch("/case4/imageForm.do")
 });
@@ -13,4 +13,26 @@ document.forms[0].addEventListener("submit",(event)=>{
 	let newImg = document.createElement("img");
 	newImg.src=`${url}?name=${imageName}`;
 	document.body.appendChild(newImg)
+});*/
+
+document.forms[0].addEventListener("submit",(event)=>{
+	event.preventDefault();
+	let url = event.target.action;
+	let imageName = event.target.name.value;
+	fetch(`${url}?name=${imageName}`)
+	    .then((response) => {
+			alert("이미지 데이터 : ",response);
+	        return response.blob(); 
+	    })
+	    .then((imageData) => {
+	        alert(imageData);
+			let imageURL = URL.createObjectURL(imageData);
+			let img = document.createElement("img");
+			img.src = imageURL;
+			document.body.appendChild(img);
+			URL.revokeObjectURL(imageData);
+	    })
+	    .catch((error) => {
+	        console.error('Error fetching image:', error);
+	    });
 });
